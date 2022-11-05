6f516f35f234237188d83676f7158258819f9939
CodeBenyamin <benyaminsegundo@gmail.com>
2022-11-05 03:52:42 -0500

    Se agrega codigo ordenar libros.

--- tarea1.py
+++ tarea1.py
@@ -83,7 +83,17 @@ def buscarISBN_titulo():
   
 
 def ordenar_titulo():
-      print("ordenar_titulo =) ")
+    lista_titulo = []
+    print("Orden de libros por titulo =) ")
+    with open("libros.csv", 'r') as ar:
+        reader = csv.reader(ar)
+        for i in reader:
+            id, titulo, genero, ISBN, editorial, autores, *a = i
+            lista_titulo.append(titulo)
+        lista_titulo.pop(0)
+        lista_titulo.sort()
+        for count, valor in enumerate(zip(lista_titulo), start=1):
+            print(count, " - ", *valor)
 
 def busc_porAutor_editorial_genero():
       print("busc_porAutor_editorial_genero =) ")
